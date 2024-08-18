1c80874856ad48431bfaadd73d695945f2a27351
Giorgi-Banetishvili <georgebanetishvili@gmail.com>
2024-08-18 10:43:41 +0400

    Add files via upload

---
+++ C# mini-project random number generator/C# mini-project random number generator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__mini_project_random_number_generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(rnd.Next(100));
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
