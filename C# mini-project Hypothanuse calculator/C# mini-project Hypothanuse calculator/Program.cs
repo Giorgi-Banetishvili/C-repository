0ab2eb54ef8dbd9f37872a699a8efe2da6b79ab6
Giorgi-Banetishvili <georgebanetishvili@gmail.com>
2024-08-18 10:46:32 +0400

    Add files via upload

---
+++ C# mini-project Hypothanuse calculator/C# mini-project Hypothanuse calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__mini_project_Hypothanuse_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+          
+          
+
+
+            Console.WriteLine("Enter side a: ");
+             double a =Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
