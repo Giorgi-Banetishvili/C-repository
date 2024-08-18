44a5235a16be978609b28973f26048f163a429b8
Giorgi-Banetishvili <georgebanetishvili@gmail.com>
2024-08-18 10:45:17 +0400

    Add files via upload

---
+++ C# mini-project temperature conversion/C# mini-project temperature conversion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__mini_project_temperature_conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          
+            Console.WriteLine("Choose F or C ");
+            char Unit = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter temperature to convert to ");
+            double Temp = Convert.ToDouble(Console.ReadLine());
+
+            if (Unit == 'C' || Unit == 'c')
+            {
+
+                Temp = (Temp - 32) / 1.8;
+                Console.WriteLine($"Temperature is {Temp} C");
+            }
+            else if (Unit == 'F' || Unit == 'f')
+            {
+                Temp = (1.8 * Temp) + 32.0;
+                Console.WriteLine($"Temperature is {Temp} F");
+            }
+            else
+            {
+                Console.WriteLine("You didnt enter a valid character!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
